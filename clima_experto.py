f3b97ff54e38f85d4ba16b8e0bf7325580174d37
Fernando Cardozo <nuevo_10000@outlook.com>
2023-10-31 21:22:38 -0300

    Agregar la posibilidad de agregar reglas adicionales

--- clima_experto.py
+++ clima_experto.py
@@ -1,4 +1,4 @@
-# Definición de la base de conocimiento (reglas)
+# Definición de la base de conocimiento inicial (reglas)
 reglas = [
     {"if": ["llueve", "frío"], "then": "Está lloviendo y hace frío"},
     {"if": ["llueve", "templado"], "then": "Está lloviendo y hace un clima agradable"},
@@ -10,6 +10,20 @@ reglas = [
     {"if": ["nublado", "templado"], "then": "Está nublado, pero con un clima agradable"},
     {"if": ["nublado", "calor"], "then": "Está nublado y hace calor"}
 ]
+
+# Pedir al usuario que ingrese reglas adicionales
+print("¿Deseas agregar reglas adicionales a la base de conocimiento?")
+print("Ingresa 'si' para agregar reglas o 'no' para continuar sin cambios.")
+respuesta = input("Respuesta: ").lower()
+
+while respuesta == 'si':
+    condiciones = input("Condiciones (e.g., soleado, frío): ").split(',')
+    resultado = input("Resultado (e.g., Hace sol, pero hace frío): ")
+    
+    regla = {"if": [condicion.strip() for condicion in condiciones], "then": resultado}
+    reglas.append(regla)
+    
+    respuesta = input("¿Deseas agregar otra regla? (si/no): ").lower()
 
 # Pedir al usuario que ingrese hechos iniciales
 print("Por favor, ingresa los hechos iniciales uno por uno (presiona Enter después de cada uno).")
